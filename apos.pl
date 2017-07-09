586d2a02f20dc390de0dfd3da79273fc48217264
Caesar <williamcesar1995@hotmail.com>
2017-07-09 13:24:24 -0300

    Update apos.pl

--- apos.pl
+++ apos.pl
@@ -33,7 +33,7 @@ nacionalidade(X,alemao):-(X=\=1),cigarro(X,prince),not(
           bebida(X,cha);bebida(X,cerveja);animal(X,passaros);
           animal(X,cachorros);cor(X,vermelha);
           cor(X,amarela)).
-nacionalidade(X,dirnamarques):-((X=\=1),(X=\=3)),bebida(X,cha),
+nacionalidade(X,dinamarques):-((X=\=1),(X=\=3)),bebida(X,cha),
           not(cigarro(X,blue_master);cigarro(X,prince);
           animal(X,cachorros);cor(X,vermelha);
           cor(X,verde)).
@@ -49,18 +49,18 @@ nacionalidade(X,sueco):-(X=\=1),animal(X,cachorros),
           not(bebida(X,cha);cigarro(X,prince);
           cigarro(X,pall_mall);cor(X,vermelha)).
 
-bebida(X,leite):-X=:=3,not(nacionalidade(X,dirnamarques);cor(X,verde);
+bebida(X,leite):-X=:=3,not(nacionalidade(X,dinamarques);cor(X,verde);
           cigarro(X,blue_master)).
-bebida(X,agua):-(X=\=3),not(nacionalidade(X,dirnamarques);cor(X,verde);
+bebida(X,agua):-(X=\=3),not(nacionalidade(X,dinamarques);cor(X,verde);
           cigarro(X,blue_master)).
-bebida(X,cafe):-(X=\=3),cor(X,verde),not(nacionalidade(X,dirnamarques);
+bebida(X,cafe):-(X=\=3),cor(X,verde),not(nacionalidade(X,dinamarques);
           nacionalidade(X,ingles);
           cigarro(X,blue_master);cigarro(X,dunhill)).
 bebida(X,cerveja):-(X=\=3),cigarro(X,blue_master),
-          not(nacionalidade(X,dirnamarques);
+          not(nacionalidade(X,dinamarques);
           cor(X,verde);cor(X,amarela);animal(X,passaros);
           cigarro(X,prince);cigarro(X,dunhill)).
-bebida(X,cha):-(X=\=3),nacionalidade(X,dirnamarques),
+bebida(X,cha):-(X=\=3),nacionalidade(X,dinamarques),
           not(nacionalidade(X,noruegues);cor(X,verde);cor(X,vermelha);
           animal(X,cachorros);cigarro(X,blue_master)).
 
@@ -88,7 +88,7 @@ cigarro(X,blends):-(((X-1)>0),(X+1)<6)),
           nacionalidade(X,alemao)).
 cigarro(X,blue_master):-(X=\=3),bebida(X,,cerveja),
           not(cor(X,amarela);animal(X,passaros);
-          nacionalidade(X,dirnamarques);
+          nacionalidade(X,dinamarques);
           nacionalidade(X,alemao);
           cor(X,verde))
 cigarro(X,pall_mall):-animal(X,passaros),
